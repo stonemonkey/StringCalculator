74fef045452e6decb0ab8650daf515d4647265b9
stonemonkey <stonemonkey77ro@yahoo.com>
2013-03-01 08:38:18 +0200

    up.

---
+++ StringCalculatorKata.Tests/CalculatorTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace StringCalculatorKata.Tests
+{
+    public class CalculatorTests
+    {
+        private Calculator _calculator;
+
+        [SetUp]
+        public void TestInit()
+        {
+            _calculator = new Calculator();
+        }
+
+        [Test]
+        public void Add_returns_0_when_input_is_empty()
+        {
+            var result = _calculator.Add(string.Empty);
+
+            Assert.AreEqual(0, result);
+        }
+
+        [Test]
+        public void Add_returns_0_when_input_is_null()
+        {
+            var result = _calculator.Add(null);
+
+            Assert.AreEqual(0, result);
+        }
+
+        [TestCase("1", 1)]
+        [TestCase("54", 54)]
+        public void Add_returns_number_when_input_is_number(string input, int expected)
+        {
+            var result = _calculator.Add(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestCase("1,2", 3)]
+        [TestCase("1,2\n3", 6)]
+        [TestCase("1,1001", 1)]
+        [TestCase("//;\n1;2\n3", 6)]
+        [TestCase("//[;]\n1;2\n3", 6)]
+        [TestCase("//[***][;]\n1;2***3", 6)]
+        public void Add_returns_sum_when_input_contains_numbers_separated_by_delimiters(string input, int expected)
+        {
+            var result = _calculator.Add(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestCase("-1,2", "negatives not allowed -1")]
+        [TestCase("-1,2,-3", "negatives not allowed -1 -3")]
+        public void Add_throws_when_input_contains_negative_numbers(string input, string negativesNotAllowed)
+        {
+            Assert.That(Assert.Throws<ApplicationException>(() => _calculator.Add(input)).Message == negativesNotAllowed);
+        }
+    }
+}
---
+++ StringCalculatorKata/Calculator.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StringCalculatorKata
+{
+    public class Calculator
+    {
+        public int Add(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return 0;
+
+            var customSeparatorsDefinition = GetCustomSeparatorsDefinition(input);
+            var numbersString = GetNumbersString(input, customSeparatorsDefinition);
+
+            return Add(customSeparatorsDefinition, numbersString);
+        }
+
+        private static int Add(string customSeparatorsDefinition, string numbersString)
+        {
+            var numbers = GetNumbers(customSeparatorsDefinition, numbersString);
+
+            var negativeNumbers = new StringBuilder();
+            var sum = numbers.Sum(number => ProcessNumber(number, negativeNumbers));
+
+            if (negativeNumbers.Length > 0)
+                throw new ApplicationException("negatives not allowed" + negativeNumbers);
+
+            return sum;
+        }
+
+        private static IEnumerable<string> GetNumbers(string customSeparatorsDefinition, string numbersString)
+        {
+            var separators = GetSeparators(customSeparatorsDefinition);
+
+            return numbersString.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        private static int ProcessNumber(string number, StringBuilder negativeNumbers)
+        {
+            var value = Convert.ToInt32(number);
+
+            if (value < 0)
+                negativeNumbers.Append(" " + number);
+            
+            if (value > 1000)
+                value = 0;
+
+            return value;
+        }
+
+        private static string GetNumbersString(string input, string customSeparatorsDefinition)
+        {
+            if (!string.IsNullOrEmpty(customSeparatorsDefinition))
+                return input.Replace(customSeparatorsDefinition, string.Empty);
+            
+            return input;
+        }
+
+        private static string GetCustomSeparatorsDefinition(string input)
+        {
+            if (input.Contains("//"))
+                return input.Substring(0, input.IndexOf('\n') + 1);
+            
+            return string.Empty;
+        }
+
+        private static string[] GetSeparators(string customSeparatorsDefinition)
+        {
+            var separators = new List<string> {",", "\n"};
+
+            if (!string.IsNullOrEmpty(customSeparatorsDefinition))
+                separators.AddRange(GetCustomSeparators(customSeparatorsDefinition));
+
+            return separators.ToArray();
+        }
+
+        private static IEnumerable<string> GetCustomSeparators(string customSeparatorsDefinition)
+        {
+            var cleanDefinition = customSeparatorsDefinition.Trim('/', '[', ']', '\n');
+
+            return  cleanDefinition.Split(new[] {"]["}, StringSplitOptions.RemoveEmptyEntries);
+        }
+    }
+}
